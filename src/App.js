eeca7d23524693a9a857180c90a77e3e3a564a83
YourGitHubUserName <your_email@example.com>
2024-04-19 00:56:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./Pages/Profile";
+import HomePage from "./Pages/HomePage";
+import List from "./Pages/list";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Profilelist" element={<List />} />
+      </Routes>
     </div>
   );
 }
